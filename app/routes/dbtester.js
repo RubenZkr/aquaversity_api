30c26ff7d0f94102c91c0778ba405bb66ec916dd
Ruben <ruben@zkr.nl>
2024-06-14 19:37:36 +0200

    fix

--- app/routes/dbtester.js
+++ app/routes/dbtester.js
@@ -1,5 +1,6 @@
 ﻿const express = require("express");
 const authenticateToken = require("../middlewares/authMiddleware");
+const db = require("../config/database");
 
 
 const router = express.Router();
@@ -7,5 +8,16 @@ const router = express.Router();
 router.get("/route", (req, res) => {
     res.send("Test route");
 });
+
+router.get("/user", async (req, res) => {
+    try {
+        const users = await db.query("SELECT * FROM users");
+
+        res.json(users);
+    } catch (error) {
+        res.status(500).json({ message: error });
+    }
+});
+
 
 module.exports = router;
